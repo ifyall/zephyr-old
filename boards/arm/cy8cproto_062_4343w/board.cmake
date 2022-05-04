979ecddff8524b71cc69ae66310915bf536dc5fa
Wheeler Keith (CYSC CSS ICW SW T 1) <keith.wheeler@infineon.com>
2022-05-04 06:53:56 +0000

    boards: arm: cy8cproto_062_4343w: Updated doc(rst) and board.cmake - updated errata and build instructions in index.rst - updated rst with programming and debug information - updated boards/arm/cy8cproto_062_4343w/board.cmake to   automatically switch to PSoC 6 CM4 core during gdb session.
    Signed-off-by: Keith Wheeler <keith.wheeler@infineon.com>

--- boards/arm/cy8cproto_062_4343w/board.cmake
+++ boards/arm/cy8cproto_062_4343w/board.cmake
@@ -1,6 +1,9 @@
 # Copyright (c) 2021 Cypress Semiconductor Corporation.
 # SPDX-License-Identifier: Apache-2.0
 
+# During gdb session, by default connect to CM4 core.
+board_runner_args(openocd "--gdb-init=disconnect")
+board_runner_args(openocd "--gdb-init=target extended-remote :3334")
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
 
